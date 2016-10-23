6ec855bca47d409a70fc9b975eb9025713e4da97
Leandro <funes.s.leandro@gmail.com>
2016-10-22 21:56:29 -0300

    Test

--- RegistradorDeOfertas/gui/data/JsonData.java
+++ RegistradorDeOfertas/gui/data/JsonData.java
@@ -14,7 +14,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import proc.Exportator;
-
+//Test
 public class JsonData {
 	
 	private JSONParser parser ;
