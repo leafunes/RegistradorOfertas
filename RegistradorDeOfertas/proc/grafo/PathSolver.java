b49c22765a4ee342ff96dc658a2369d5f46defbd
leafunes <funes.s.leandro@gmail.com>
2016-11-03 19:32:25 -0300

    Se completo la clase PathSolver

--- RegistradorDeOfertas/proc/grafo/PathSolver.java
+++ RegistradorDeOfertas/proc/grafo/PathSolver.java
@@ -1,9 +1,41 @@
 package grafo;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PathSolver{
+	
+	private class Nodo <T extends Distanciable<T>>{
+		
+		boolean visitado;
+		T vertice;
+		Nodo<T> anterior;
+		double distanciaTent;
+		
+		public Nodo(T vertice){
+			this.vertice = vertice;
+			visitado = false;
+			distanciaTent = Double.POSITIVE_INFINITY;
+			
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			
+			Nodo<T> other = (Nodo<T>) obj;//TODO
+			
+			return vertice.equals(other.vertice);
+		}
+		
+	}
 	
 	private static PathSolver current;
 	
@@ -24,68 +56,96 @@ public class PathSolver{
 		if(!graph.getVerticesSet().contains(source) && !graph.getVerticesSet().contains(destination))
 			throw new IllegalArgumentException("Los vertices no pertenecen al grafo");
 		
-		double[] distancias = new double[graph.vertices];
-		boolean[] visitados = new boolean[graph.vertices];
+		List < Nodo<T> > nodos = createNodos(source, destination, graph);
 		
-		List<T> vertices = graph.refList;
 		
-		int sourceIndex = vertices.indexOf(source);
-		int destIndex = vertices.indexOf(destination);
-		
-		for (int i = 0; i < graph.getVertices(); i++) {
+		while(!estanTodosVistos(nodos)){
 			
-			if(graph.containsEdge(source, graph.getVertex(i)) == false)
-				distancias[i] = Double.POSITIVE_INFINITY;
-			
-			else
-				distancias[i] = graph.getWeigth(source, graph.getVertex(i));
+			Nodo<T> minimo = getMinimoNoVisitado(nodos);
+			minimo.visitado = true;
+			actualizaDistancia(minimo, nodos, graph);
 			
 		}
 		
-		distancias[sourceIndex] = 0;
-		visitados[sourceIndex] = true;
+		Nodo <T> destNodo = new Nodo<T>(destination);
+		List<T> secuencia = new ArrayList<T>();
 		
-		while(!visitedAll(visitados)){
-			T vertice = minimoSinVisitar(visitados, distancias, vertices);
-			
-			int verticeIndex = vertices.indexOf(vertice);
-			visitados[verticeIndex] = true;
-			for (T t : graph.getNehiVertex(vertice)) {
-				
-				int tIndex = vertices.indexOf(t);
-				if(distancias[tIndex] > distancias[verticeIndex] + graph.getWeigth(vertice, t))
-					distancias[tIndex] = distancias[verticeIndex] + graph.getWeigth(vertice, t);
-				
-			}
-		}
+		generateSecuencia(destNodo,secuencia);
 		
+		return secuencia;
 		
 	}
 	
-	private boolean visitedAll(boolean[] visitados){
-		for (boolean visitado : visitados) {
-			if(!visitado) return false;
+	<T extends Distanciable<T>> List <Nodo<T>> createNodos(T source, T destination, DiGraph<T> graph){
+		
+		List< Nodo<T> > nodos = new ArrayList<>();
+		
+		//Icicializo el primer nodo(source)
+		Nodo <T> sourceNodo = new Nodo<T>(source);
+		sourceNodo.anterior = null;
+		sourceNodo.visitado = true;
+		sourceNodo.distanciaTent = 0;
+		nodos.add(sourceNodo);
+		
+		//Creo todos los nodos que no sean ni el source ni el destination
+		for(T vertice : graph.getVerticesSet()){
+			if(!vertice.equals(source) && !vertice.equals(destination)){
+				Nodo<T> toAdd = new Nodo<T>(vertice);
+				nodos.add(toAdd);
+			}
 		}
+		
+		//Creo el ultio nodo (destination)
+		Nodo <T> destNodo = new Nodo<T>(destination);
+		nodos.add(destNodo);
+		
+		//Actualizo las distancias al primer nodo
+		for(Nodo<T> nodo : nodos)if(graph.containsEdge(source, nodo.vertice))
+			nodo.distanciaTent = graph.getWeigth(source, nodo.vertice);
+		
+		return nodos;
+	}
+	
+	<T extends Distanciable<T>> boolean estanTodosVistos(List<Nodo<T>> nodos){
+		
+		for (Nodo<T> nodo : nodos)
+			if(!nodo.visitado)
+				return false;
 		
 		return true;
+		
 	}
 	
-	private <T extends Distanciable<T>> T minimoSinVisitar(boolean[] visitados, double[] distancias, List<T> refs){
-		double min = Double.MIN_VALUE;
-		T vertexMin = null;
+	<T extends Distanciable<T>> Nodo<T> getMinimoNoVisitado(List<Nodo<T>> nodos){
 		
-		for (int i = 0; i < distancias.length; i++) {
-			double dist = distancias[i];
-			boolean visitado = visitados[i];
+		Nodo<T> min = nodos.get(0);
+		
+		for (Nodo<T> nodo : nodos)
+			if(!nodo.visitado && min.distanciaTent < nodo.distanciaTent)
+				min = nodo;
 			
-			if(dist < min && !visitado){
-				min = dist;
-				vertexMin = refs.get(i);
-			}
-			
+		return min;
+	}
+	
+	<T extends Distanciable<T>> void actualizaDistancia(Nodo<T> nodo, List<Nodo<T>> nodosList, DiGraph<T> grafo){
+		
+		for(Nodo<T> vecino : nodosList)
+			if(grafo.containsEdge(nodo.vertice, vecino.vertice))
+				if(vecino.distanciaTent > nodo.distanciaTent + grafo.getWeigth(nodo.vertice, vecino.vertice)){
+					vecino.distanciaTent = nodo.distanciaTent + grafo.getWeigth(nodo.vertice, vecino.vertice);
+					vecino.anterior = nodo;
+				}
+		
+	}
+	
+	<T extends Distanciable<T>> void generateSecuencia(Nodo<T> dest, List<T> secuencia){
+		if(dest == null)
+			Collections.reverse(secuencia);
+		
+		else{
+			secuencia.add(dest.vertice);
+			generateSecuencia(dest.anterior, secuencia);
 		}
-		
-		//TODO
 	}
 	
 }
